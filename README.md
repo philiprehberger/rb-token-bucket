373ff872f9666539e80561cb6a0af75768c68528
Reverend Philip <me@philiprehberger.com>
2014-02-01 14:19:56 -0600

    Initial release: philiprehberger-token-bucket

---
+++ README.md
@@ -0,0 +1,81 @@
+# philiprehberger-token_bucket
+
+[![Tests](https://github.com/philiprehberger/rb-token-bucket/actions/workflows/ci.yml/badge.svg)](https://github.com/philiprehberger/rb-token-bucket/actions/workflows/ci.yml)
+[![Gem Version](https://badge.fury.io/rb/philiprehberger-token_bucket.svg)](https://rubygems.org/gems/philiprehberger-token_bucket)
+[![License](https://img.shields.io/github/license/philiprehberger/rb-token-bucket)](LICENSE)
+
+Token bucket rate limiter with configurable capacity and refill rate
+
+## Requirements
+
+- Ruby >= 3.1
+
+## Installation
+
+Add to your Gemfile:
+
+```ruby
+gem 'philiprehberger-token_bucket'
+```
+
+Or install directly:
+
+```bash
+gem install philiprehberger-token_bucket
+```
+
+## Usage
+
+```ruby
+require 'philiprehberger/token_bucket'
+
+bucket = Philiprehberger::TokenBucket::Bucket.new(capacity: 10, refill_rate: 5)
+```
+
+### Blocking Take
+
+```ruby
+bucket.take(3)    # blocks until 3 tokens are available, then consumes them
+bucket.take       # takes 1 token by default
+```
+
+### Non-blocking Take
+
+```ruby
+if bucket.try_take(2)
+  # tokens acquired, proceed
+else
+  # not enough tokens, try again later
+end
+```
+
+### Checking Availability
+
+```ruby
+bucket.available   # => current number of tokens (Float)
+bucket.wait_time(5) # => seconds until 5 tokens will be available
+```
+
+## API
+
+### `Philiprehberger::TokenBucket::Bucket`
+
+| Method | Description |
+|--------|-------------|
+| `.new(capacity:, refill_rate:)` | Create a bucket with max tokens and tokens-per-second refill |
+| `#take(n = 1)` | Block until n tokens are available, then consume them |
+| `#try_take(n = 1)` | Consume n tokens if available, return true/false |
+| `#available` | Return the current number of available tokens |
+| `#wait_time(n = 1)` | Estimate seconds until n tokens will be available |
+
+## Development
+
+```bash
+bundle install
+bundle exec rspec
+bundle exec rubocop
+```
+
+## License
+
+MIT
---
+++ lib/philiprehberger/token_bucket.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require_relative 'token_bucket/version'
+
+module Philiprehberger
+  module TokenBucket
+    class Error < StandardError; end
+
+    # A thread-safe token bucket rate limiter
+    class Bucket
+      # @param capacity [Numeric] maximum number of tokens
+      # @param refill_rate [Numeric] tokens added per second
+      def initialize(capacity:, refill_rate:)
+        raise Error, 'capacity must be positive' unless capacity.positive?
+        raise Error, 'refill_rate must be positive' unless refill_rate.positive?
+
+        @capacity = capacity.to_f
+        @refill_rate = refill_rate.to_f
+        @tokens = @capacity
+        @last_refill = now
+        @mutex = Mutex.new
+      end
+
+      # Take n tokens, blocking until they are available
+      #
+      # @param n [Numeric] number of tokens to take
+      # @return [void]
+      # @raise [Error] if n exceeds capacity
+      def take(n = 1)
+        raise Error, "cannot take #{n} tokens from bucket with capacity #{@capacity}" if n > @capacity
+
+        loop do
+          wait = nil
+          @mutex.synchronize do
+            refill
+            if @tokens >= n
+              @tokens -= n
+              return
+            end
+            wait = wait_time(n)
+          end
+          sleep(wait)
+        end
+      end
+
+      # Try to take n tokens without blocking
+      #
+      # @param n [Numeric] number of tokens to take
+      # @return [Boolean] true if tokens were taken, false otherwise
+      def try_take(n = 1)
+        @mutex.synchronize do
+          refill
+          if @tokens >= n
+            @tokens -= n
+            true
+          else
+            false
+          end
+        end
+      end
+
+      # Return the number of currently available tokens
+      #
+      # @return [Float] available tokens
+      def available
+        @mutex.synchronize do
+          refill
+          @tokens
+        end
+      end
+
+      # Calculate how long to wait for n tokens to become available
+      #
+      # @param n [Numeric] number of tokens needed
+      # @return [Float] seconds to wait (0.0 if tokens are already available)
+      def wait_time(n = 1)
+        @mutex.synchronize do
+          refill
+          deficit = n - @tokens
+          return 0.0 if deficit <= 0
+
+          deficit / @refill_rate
+        end
+      end
+
+      private
+
+      def refill
+        current = now
+        elapsed = current - @last_refill
+        @tokens = [@tokens + elapsed * @refill_rate, @capacity].min
+        @last_refill = current
+      end
+
+      def now
+        Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      end
+    end
+  end
+end
